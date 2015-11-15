392be3046481550284dd434426bd11c643a65e5c
Renoir <sinmatonesaporfavor@gmail.com>
2015-11-15 14:05:36 -0600

    Rotar imagen

---
+++ Rotation.java
@@ -0,0 +1,67 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+
+public class MyGdxGame extends ApplicationAdapter {
+
+    /* Para mostrar graficos en 2D es la vista del jugador */
+	private OrthographicCamera camera;
+    /* Aquí se manda todos los comandos de dibujo a LIBGDX*/
+    private SpriteBatch batch;
+
+    private Texture texture;
+    private Sprite sprite;
+    private float rot;
+
+
+	@Override
+	public void create () {
+        float w = Gdx.graphics.getWidth();
+        float h = Gdx.graphics.getHeight();
+
+        camera = new OrthographicCamera(1, h/w);
+        batch = new SpriteBatch();
+
+        texture = new Texture(Gdx.files.internal("badlogic.jpg"));
+        texture.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
+
+        TextureRegion region = new TextureRegion(texture, 0, 0, 512, 275);
+
+        sprite = new Sprite(region);
+        sprite.setSize(0.9f, 0.9f * sprite.getHeight() / sprite.getWidth());
+        sprite.setOrigin(sprite.getWidth() / 2, sprite.getHeight() / 2);
+        sprite.setPosition(-sprite.getWidth()/2, -sprite.getHeight()/2);
+	}
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        texture.dispose();
+    }
+
+	@Override
+	public void render () {
+        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        batch.setProjectionMatrix(camera.combined);
+        batch.begin();
+        final float degreesPerSecond = 20.0f;
+        rot = (rot + Gdx.graphics.getDeltaTime() * degreesPerSecond % 360);
+        sprite.setRotation(rot);
+        sprite.draw(batch);
+        batch.end();
+	}
+}
